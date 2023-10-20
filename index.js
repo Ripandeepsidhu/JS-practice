371aef406b88bb7ebdb99a1457284cf737924089
Ripandeepsidhu <kaursidhuripandeep@gmail.com>
2023-10-20 14:02:20 +0200

    content added

---
+++ index.js
@@ -0,0 +1,53 @@
+let base = 10 ;
+let height = 13 ;
+let a;
+console.log((a = base * height)/2);
+
+
+base = 16.5 ;
+height = 20.3 ;
+let b;
+console.log((b = base * height)/2);
+
+if(a>=b){
+    console.log("Triangle one is bigger");
+}
+else{
+    console.log("Triangle two is bigger");
+}
+
+base = 16.5 ;
+height = 20.3 ;
+let c;
+console.log((c = base * height)/2);
+
+base = 20.3 ;
+height = 16.5 ;
+let x;
+console.log((x = base * height)/2);
+if(c>=x){
+    console.log("Triangle four is bigger");
+}
+else{
+    console.log("Triangle five is bigger");
+}
+
+base = 7.8 ;
+height = 5.6 ;
+let y;
+console.log((y = base * height)/2);
+
+base = 9.3 ;
+height = 12.4 ;
+let z;
+console.log((z = base * height)/2);
+if(y>=z){
+    console.log("Triangle five is bigger");
+}
+else{
+    console.log("Triangle six is bigger");
+}
+
+
+
+
